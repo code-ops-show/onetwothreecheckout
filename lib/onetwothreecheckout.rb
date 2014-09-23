9ff07630a91506dab6ecaf8eefdf328d146a300a
Zack Siri <zack@codemy.net>
2014-09-23 15:16:08 +0700

    Added channels code

--- lib/onetwothreecheckout.rb
+++ lib/onetwothreecheckout.rb
@@ -6,20 +6,28 @@ module Onetwothreecheckout
   extend Config
 
   AGENT_CODES = {
-    'BBL' => 'Bangkok Bank'
-    'KBANK' => 'Kasikorn Bank'
-    'KTB' => 'Krung Thai Bank'
-    'SCB' => 'Siam Commercial Bank'
-    'TBANK' => 'Thanachart Bank Public Company Ltd.'
-    'UOB' => 'United Overseas Bank'
-    'CIMB' => 'CIMB Thai Bank Public Company Ltd.'
-    'TMB' => 'TMB Bank Public Company Ltd.'
-    'BAY' => 'Bank of Ayutthaya'
-    '7ELEVEN' => '7 Eleven Counter Service'
-    'TRUEMONEY' => 'True Money Shop'
-    'TESCO' => 'Tesco Lotus Counter Service'
-    'TOT' => 'Just Pay by TOT public company Ltd.'
-    'PAP' => 'Pay at Post'
+    'Bangkok Bank'                        => 'BBL',
+    'Kasikorn Bank'                       => 'KBANK',
+    'Krung Thai Bank'                     => 'KTB',
+    'Siam Commercial Bank'                => 'SCB',
+    'Thanachart Bank Public Company Ltd.' => 'TBANK',
+    'United Overseas Bank'                => 'UOB',
+    'CIMB Thai Bank Public Company Ltd.'  => 'CIMB',
+    'TMB Bank Public Company Ltd.'        => 'TMB',
+    'Bank of Ayutthaya'                   => 'BAY',
+    '7 Eleven Counter Service'            => '7ELEVEN',
+    'True Money Shop'                     => 'TRUEMONEY',
+    'Tesco Lotus Counter Service'         => 'TESCO',
+    'Just Pay by TOT public company Ltd.' => 'TOT',
+    'Pay at Post'                         => 'PAP'
+  }
+
+  CHANNEL_CODES = { 
+    'ATM Machine'                       => 'ATM',
+    'Bank Branch Counter'               => 'BANKCOUNTER',
+    'Internet Banking'                  => 'IBANKING',
+    'Web Payment of Banking'            => 'WEBPAY',
+    '7 Eleven, True Money, Tesco Lotus' => "OVERTHECOUNTER"
   }
 
   def self.root
